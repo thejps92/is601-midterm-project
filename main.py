2210278df7dc0ed9aff5148490bee813c4d7ca97
John Scognomillo <jps92@njit.edu>
2026-02-23 21:48:16 -0500

    Update project files for new features

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from app.calculator import calculator
-
-def main():
-    calculator()
+from app.calculator_repl import calculator_repl
 
 if __name__ == "__main__":
-    main()
+    calculator_repl()
